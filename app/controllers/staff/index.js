317298465ed3189600c5420512faeb47a1fe86bf
shubhsherl <shubham.singh094@yahoo.com>
2019-06-21 07:02:20 +0530

    remove invite model

--- app/controllers/staff/index.js
+++ app/controllers/staff/index.js
@@ -12,7 +12,6 @@ export default Controller.extend({
 
     showInviteUserModal: false,
 
-    inviteOrder: null,
     userOrder: null,
 
     init() {
@@ -23,17 +22,8 @@ export default Controller.extend({
 
     currentUser: alias('model'),
 
-    sortedInvites: sort('filteredInvites', 'inviteOrder'),
     sortedActiveUsers: sort('activeUsers', 'userOrder'),
     sortedSuspendedUsers: sort('suspendedUsers', 'userOrder'),
-
-    invites: computed(function () {
-        return this.store.peekAll('invite');
-    }),
-
-    filteredInvites: computed('invites.@each.isNew', function () {
-        return this.invites.filterBy('isNew', false);
-    }),
 
     allUsers: computed(function () {
         return this.store.peekAll('user');
@@ -70,22 +60,5 @@ export default Controller.extend({
 
     fetchUsers: task(function* () {
         yield this.store.query('user', {limit: 'all'});
-    }),
-
-    fetchInvites: task(function* () {
-        if (this.currentUser.isAuthorOrContributor) {
-            return;
-        }
-
-        // ensure roles are loaded before invites. Invites do not have embedded
-        // role records which means Ember Data will try to fetch the roles
-        // automatically when invite.role is queried, loading roles first makes
-        // them available in memory and cuts down on network noise
-        let knownRoles = this.store.peekAll('role');
-        if (knownRoles.length <= 1) {
-            yield this.store.query('role', {limit: 'all'});
-        }
-
-        return yield this.store.query('invite', {limit: 'all'});
     })
 });
